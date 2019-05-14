4f6933ef4711c982ce49f52a33c40b19383a7c8d
Muhammadfathul <46747652+muhammadfathul@users.noreply.github.com>
2019-05-14 21:38:28 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,12 @@
+clear
+echo
+apt update && apt upgrade
+apt install ruby
+apt install curl
+apt install figlet
+gem install lolcat
+termux-setup-storage
+cd storage/shared
+mkdir virtex
+cd $HOME
+echo
